2da7450bf8491b5922ba88975c08e7d4729d14bb
vlad bronnikov <bronnikovvd@gmail.com>
2026-08-23 02:35:25 +0300

    fix: broke the interface down into components

---
+++ src/components/Field.jsx
@@ -0,0 +1,12 @@
+const Field = () => {
+    return (
+        <div className="todo__field field">
+            <label className="field__label" htmlFor="new-task">
+                New task
+            </label>
+            <input className="field__input" id="new-task" placeholder=" " autoComplete="off" />
+        </div>
+    )
+}
+
+export default Field
